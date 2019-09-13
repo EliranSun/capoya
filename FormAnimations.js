7e1af5f816dd3a66db8089a13adb782ddd86f76f
Eliran Shemesh <eliran.shemesh@netomedia.com>
2019-09-13 09:20:00 +0300

    init commit for the legacy eliran

---
+++ FormAnimations.js
@@ -0,0 +1,516 @@
+  $(document).ready(function(){    
+         
+		   $('.anime00').click(function(){ 
+		   		
+				$('.anime0').removeClass('animated slideInDown yesQB');
+                $('.anime0').addClass('animated slideOutUp');
+				$('.anime00').removeClass('animated slideInDown yesQB');
+                $('.anime00').addClass('animated slideOutUp');
+				
+				$('.testingPre').removeClass('yesQ');
+				$('.testingPre').addClass('noneQ');
+				
+				$('.testing').removeClass('noneQ');
+				$('.testing').addClass('yesQ');
+				
+				$('#testing00').removeClass('noneQ');
+				$('#testing00').addClass('yesQ');
+				
+				$('#testing0').removeClass('yesQ');
+				$('#testing0').addClass('noneQ');
+				
+				$('#testing0A').removeClass('yesQ');
+				$('#testing0A').addClass('noneQ');
+				
+				$('#testing0B').removeClass('yesQ');
+				$('#testing0B').addClass('noneQ');
+				
+				$('#back0').removeClass('noneQ');
+				$('#back0').addClass('yesQT');
+				
+				
+				$('.anime').removeClass('animated slideOutDown noneQ');
+				$('.anime').addClass('animated slideInUp yesQB');
+		       }); 
+			   
+		   $('.anime0').click(function(){ 
+		   		
+				$('.anime0').removeClass('animated slideInDown yesQB');
+                $('.anime0').addClass('animated slideOutUp noneQ');
+				$('.anime00').removeClass('animated slideInDown yesQB');
+                $('.anime00').addClass('animated slideOutUp');
+				
+				$('.testingPre').removeClass('yesQ');
+				$('.testingPre').addClass('noneQ');
+				
+				$('.CampOpt').removeClass('noneQ');
+				$('.CampOpt').addClass('yesQ');
+				
+				$('#testing00').removeClass('noneQ');
+				$('#testing00').addClass('yesQ');
+				
+				$('#testing0').removeClass('yesQ');
+				$('#testing0').addClass('noneQ');
+				
+				$('#testing0A').removeClass('yesQ');
+				$('#testing0A').addClass('noneQ');
+				
+				$('#testing0B').removeClass('yesQ');
+				$('#testing0B').addClass('noneQ');
+				
+				$('#back0').removeClass('noneQ');
+				$('#back0').addClass('yesQT');
+				
+				
+				$('.anime0B').removeClass('animated slideOutDown noneQ');
+				$('.anime0B').addClass('animated slideInUp yesQB');
+		       }); 
+			   
+			 	 $('.anime0B').click(function(){ 
+              	
+                $('.anime0B').removeClass('animated slideInUp yesQ yesQB yesQT');
+				$('.anime0B').addClass('animated slideOutUp noneQ');
+				
+				$('#testingPre2').removeClass('yesQ yesQT yesQB');
+				$('#testingPre2').addClass('noneQ');
+				
+				$('#testing6').removeClass('noneQ');
+				$('#testing6').addClass('yesQT');
+				
+				$('.anime6').removeClass('animated slideOutDown noneQ');
+				$('.anime6').addClass('animated slideInUp2 yesQB');
+				
+				$('#testing00').removeClass('yesQ yesQT yesQB');
+				$('#testing00').addClass('noneQ');
+				
+				$('#back0').removeClass('yesQ yesQT yesQB');
+				$('#back0').addClass('noneQ');
+				
+				$('#testing0').removeClass('yesQ');
+				$('#testing0').addClass('noneQ');
+				
+				$('#testingA').removeClass('yesQ');
+				$('#testingA').addClass('noneQ');
+				
+				$('#testing0B').removeClass('yesQ');
+				$('#testing0B').addClass('noneQ');
+				
+				$('#testing0C').removeClass('yesQ');
+				$('#testing0C').addClass('noneQ');
+				
+				$('#testing0D2').removeClass('noneQ');
+				$('#testing0D2').addClass('yesQ');
+			
+				$('#back5B').removeClass('noneQ');
+				$('#back5B').addClass('yesQT');
+				  }); 
+			   
+			   
+           $('.anime').click(function(){ 
+              
+				$('.anime').removeClass('animated slideInDown yesQ');
+                $('.anime').addClass('animated slideOutUp noneQ');
+				
+				$('.testing').removeClass('yesQ');
+				$('.testing').addClass('noneQ');
+				
+				$('#testing2').removeClass('noneQ');
+				$('#testing2').addClass('yesQ');
+				
+				$('#testing0').removeClass('noneQ');
+				$('#testing0').addClass('yesQ');
+				
+				$('#testing00').removeClass('yesQT');
+				$('#testing00').addClass('noneQ');
+				
+				$('#testing0A').removeClass('yesQ');
+				$('#testing0A').addClass('noneQ');
+				
+				$('#testing0B').removeClass('yesQ');
+				$('#testing0B').addClass('noneQ');
+				
+				$('#back').removeClass('noneQ');
+				$('#back').addClass('yesQT');
+				
+				$('#back0').removeClass('yesQT');
+				$('#back0').addClass('noneQT');
+				
+				
+				$('.anime2').removeClass('animated slideOutDown noneQ');
+				$('.anime2').addClass('animated slideInUp yesQB');
+				
+		
+            }); 
+			
+			
+				
+			   $('#back0').click(function(){ 
+              	
+                $('.anime0').removeClass('animated slideOutUp noneQ');
+				$('.anime0').addClass('animated slideInDown yesQB');
+				
+				$('.anime00').removeClass('animated slideOutUp noneQ');
+				$('.anime00').addClass('animated slideInDown yesQB');
+				
+				$('.testingPre').removeClass('noneQ');
+				$('.testingPre').addClass('yesQ');
+				
+				$('.testing').removeClass('yesQ');
+				$('.testing').addClass('noneQ');
+				$('.testingPre2').removeClass('yesQ');
+				$('.testingPre2').addClass('noneQ');
+				
+				
+				$('#testing0').removeClass('yesQ');
+				$('#testing0').addClass('noneQ');
+				
+				$('#testing00').removeClass('yesQT');
+				$('#testing00').addClass('noneQ');
+				
+				$('.anime').removeClass('animated slideInUp yesQ');
+				$('.anime').addClass('animated slideOutDown noneQ');
+				
+				$('.anime0B').removeClass('animated slideInUp yesQB');
+				$('.anime0B').addClass('animated slideOutDown noneQ');
+				
+				$('#back0').removeClass('yesQT');
+				$('#back0').addClass('noneQ');
+				
+			   });
+			   
+			   
+			    $('#back').click(function(){ 
+              	
+                $('.anime').removeClass('animated slideOutUp slideInUp noneQ');
+				$('.anime').addClass('animated slideInDown yesQB');
+				
+				$('.testing').removeClass('noneQ');
+				$('.testing').addClass('yesQ');
+				
+				$('#testing2').removeClass('yesQT yesQ');
+				$('#testing2').addClass('noneQ');
+				
+				
+				$('#testing0').removeClass('yesQ');
+				$('#testing0').addClass('noneQ');
+				
+				$('.anime2').removeClass('animated slideInUp yesQB');
+				$('.anime2').addClass('animated slideOutDown noneQ');
+				
+				$('#back').removeClass('yesQT');
+				$('#back').addClass('noneQ');
+				
+				$('#testing00').removeClass('noneQ');
+				$('#testing00').addClass('yesQT');
+				
+				$('#back0').removeClass('noneQ');
+				$('#back0').addClass('yesQT');
+				
+				
+            }); 
+			
+			  $('.anime2').click(function(){ 
+              	
+                $('.anime2').removeClass('animated slideInUp');
+				$('.anime2').addClass('animated slideOutUp');
+				
+				$('#testing3').removeClass('noneQ');
+				$('#testing3').addClass('yesQ');
+				
+				$('#testing2').removeClass('yesQT');
+				$('#testing2').addClass('noneQ');
+				
+				
+				$('#testing3').removeClass('noneQ');
+				$('#testing3').addClass('yesQT');
+				
+				$('.anime3').removeClass('animated slideOutDown');
+				$('.anime3').addClass('animated slideInUp yesQB');
+				
+				$('#testing0').removeClass('yesQ');
+				$('#testing0').addClass('noneQ');
+				
+				$('#testing0A').removeClass('noneQ');
+				$('#testing0A').addClass('yesQ');
+				
+				$('#back2').removeClass('noneQ');
+				$('#back2').addClass('yesQT');
+				
+				
+				$('#testing0B').removeClass('yesQ');
+				$('#testing0B').addClass('noneQ');
+				
+            }); 
+			
+			    $('#back2').click(function(){ 
+				
+              
+				
+                $('.anime2').removeClass('animated slideOutUp');
+				$('.anime2').addClass('animated slideInDown');
+				
+				$('.anime3').removeClass('animated slideInUp yesQB');
+				$('.anime3').addClass('animated slideOutDown');
+				
+				$('#testing2').removeClass('noneQ');
+				$('#testing2').addClass('yesQT');
+				
+				$('#testing3').removeClass('yesQT yesQ');
+				$('#testing3').addClass('noneQ');
+				
+				
+				$('#testing0A').removeClass('yesQ');
+				$('#testing0A').addClass('noneQ');
+				
+				$('#testing0').removeClass('noneQ');
+				$('#testing0').addClass('yesQ');
+				
+				
+				
+				$('#back2').addClass('noneQ');
+				$('#back2').removeClass('yesQT');
+				
+				
+				
+            }); 
+			
+			 $('#choose').click(function(){ 
+              	
+                $('.anime3').removeClass('animated slideInUp yesQT');
+				$('.anime3').addClass('animated slideOutUp noneQ');
+				
+				
+			
+				$('#testing3').removeClass('yesQT');
+				$('#testing3').addClass('noneQ');
+				
+				$('#testing4').removeClass('noneQ');
+				$('#testing4').addClass('yesQ');
+				
+				$('.anime4').removeClass('animated slideOutDown noneQ');
+				$('.anime4').addClass('animated slideInUp2 yesQT');
+				
+				$('#testing0A').removeClass('yesQ');
+				$('#testing0A').addClass('noneQ');
+				
+				$('#testing0B').removeClass('noneQ');
+				$('#testing0B').addClass('yesQ');
+				
+				$('#testing0').removeClass('yesQ');
+				$('#testing0').addClass('noneQ');
+				
+				$('#back3').removeClass('noneQ');
+				$('#back3').addClass('yesQT');
+				
+			
+				
+            }); 
+			    $('#back3').click(function(){ 
+              	
+                $('.anime3').removeClass('animated slideOutUp noneQ');
+				$('.anime3').addClass('animated slideInDown yesQB');
+				
+				$('#testing4').removeClass('yesQ');
+				$('#testing4').addClass('noneQ');
+				
+				$('#testing3').removeClass('noneQ');
+				$('#testing3').addClass('yesQT');
+				
+				
+				$('#testing0B').removeClass('yesQ');
+				$('#testing0B').addClass('noneQ');
+				
+				$('#testing0A').removeClass('noneQ');
+				$('#testing0A').addClass('yesQ');
+				
+				$('.anime4').removeClass('animated slideInUp');
+				$('.anime4').addClass('animated slideOutDown');
+				
+				$('#back3').addClass('noneQT');
+				
+				
+            }); 
+			
+			 $('#choose2').click(function(){ 
+              	
+                $('.anime4').removeClass('animated slideInUp yesQB');
+				$('.anime4').addClass('animated slideOutUp');
+				
+				$('#testing4').removeClass('yesQT');
+				$('#testing4').addClass('noneQ');
+				
+				$('#testing5').removeClass('noneQ');
+				$('#testing5').addClass('yesQT');
+				
+				$('.anime5').removeClass('animated slideOutDown noneQ');
+				$('.anime5').addClass('animated slideInUp2 yesQB');
+				
+				$('#testing0').removeClass('yesQ');
+				$('#testing0').addClass('noneQ');
+				
+				$('#testingA').removeClass('yesQ');
+				$('#testingA').addClass('noneQ');
+				
+				$('#testing0B').removeClass('yesQ');
+				$('#testing0B').addClass('noneQ');
+				
+				$('#testing0C').removeClass('noneQ');
+				$('#testing0C').addClass('yesQ');
+			
+				$('#back4').removeClass('noneQ');
+				$('#back4').addClass('yesQT');
+				
+			
+				
+            }); 
+		  
+		  $('#back4').click(function(){ 
+              	
+                $('.anime4').removeClass('animated slideOutUp slideInUp2 yesQB');
+				$('.anime4').addClass('animated slideInDown noneQ');
+				
+				$('#testing5').removeClass('yesQT');
+				$('#testing5').addClass('noneQ');
+				
+				$('#testing4').removeClass('noneQ');
+				$('#testing4').addClass('yesQ');
+				
+				
+				$('#testing0').removeClass('yesQ');
+				$('#testing0').addClass('noneQ');
+				
+				$('#testing0A').removeClass('yesQ');
+				$('#testing0A').addClass('noneQ');
+				
+				$('#testing0C').removeClass('yesQ');
+				$('#testing0C').addClass('noneQ');
+				
+				$('#testing0B').removeClass('noneQ');
+				$('#testing0B').addClass('yesQ');
+				
+				$('.anime5').removeClass('animated slideInUp2');
+				$('.anime5').addClass('animated slideOutDown');
+				
+				$('#back4').removeClass('yesQT');
+				$('#back4').addClass('noneQ');
+				
+				
+            }); 
+			
+					
+			 $('#next').click(function(){ 
+              	
+                $('.anime5').removeClass('animated slideInUp yesQB');
+				$('.anime5').addClass('animated slideOutUp noneQ');
+				
+				$('#testing5').removeClass('yesQT');
+				$('#testing5').addClass('noneQ');
+				
+				$('#testing6').removeClass('noneQ');
+				$('#testing6').addClass('yesQT');
+				
+				$('.anime6').removeClass('animated slideOutDown noneQ');
+				$('.anime6').addClass('animated slideInUp2 yesQB');
+				
+				$('#testing0').removeClass('yesQ');
+				$('#testing0').addClass('noneQ');
+				
+				$('#testingA').removeClass('yesQ');
+				$('#testingA').addClass('noneQ');
+				
+				$('#testing0B').removeClass('yesQ');
+				$('#testing0B').addClass('noneQ');
+				
+				$('#testing0C').removeClass('yesQ');
+				$('#testing0C').addClass('noneQ');
+				
+				$('#testing0D').removeClass('noneQ');
+				$('#testing0D').addClass('yesQ');
+			
+				$('#back5').removeClass('noneQ');
+				$('#back5').addClass('yesQT');
+				
+			
+				
+            }); 
+		  $('#back5').click(function(){ 
+              	
+                $('.anime5').removeClass('animated slideOutUp slideInUp2 noneQ');
+				$('.anime5').addClass('animated slideInDown yesQB');
+				
+				$('#testing6').removeClass('yesQT');
+				$('#testing6').addClass('noneQ');
+				
+				$('#testing5').removeClass('noneQ');
+				$('#testing5').addClass('yesQT');
+				
+				
+				$('#testing0').removeClass('yesQ');
+				$('#testing0').addClass('noneQ');
+				
+				$('#testing0A').removeClass('yesQ');
+				$('#testing0A').addClass('noneQ');
+				
+				$('#testing0B').removeClass('yesQ');
+				$('#testing0B').addClass('noneQ');
+				
+				$('#testing0D').removeClass('yesQ');
+				$('#testing0D').addClass('noneQ');
+				
+				$('#testing0C').removeClass('noneQ');
+				$('#testing0C').addClass('yesQ');
+				
+				$('.anime6').removeClass('animated slideInUp yesQB');
+				$('.anime6').addClass('animated slideOutDown noneQ');
+				
+				$('#back5').removeClass('yesQT');
+				$('#back5').addClass('noneQ');
+				
+            }); 
+			
+			  $('#back5B').click(function(){ 
+              	
+                $('.anime0B').removeClass('animated slideOutUp slideInUp2 noneQ');
+				$('.anime0B').addClass('animated slideInDown yesQB');
+				
+				$('#testing6').removeClass('yesQT');
+				$('#testing6').addClass('noneQ');
+				
+				$('#testingPre2').removeClass('noneQ');
+				$('#testingPre2').addClass('yesQ');
+				
+				
+				$('#testing00').removeClass('noneQ');
+				$('#testing00').addClass('yesQ');
+				
+				$('#testing0').removeClass('noneQ');
+				$('#testing0').addClass('yesQ');
+				
+				$('#testing0A').removeClass('yesQ');
+				$('#testing0A').addClass('noneQ');
+				
+				$('#testing0B').removeClass('yesQ');
+				$('#testing0B').addClass('noneQ');
+				
+				$('#testing0D').removeClass('yesQ');
+				$('#testing0D').addClass('noneQ');
+				
+				$('#testing0C').removeClass('yesQ');
+				$('#testing0C').addClass('noneQ');
+				
+				$('#testing0D2').removeClass('yesQ');
+				$('#testing0D2').addClass('noneQ');
+				
+				
+				$('.anime6').removeClass('animated slideInUp yesQB');
+				$('.anime6').addClass('animated slideOutDown noneQ');
+				
+				$('#back5B').removeClass('yesQT');
+				$('#back5B').addClass('noneQ');
+				
+				$('#back0').removeClass('noneQ');
+				$('#back0').addClass('yesQT');
+				
+            }); 
+   
+     }); //window// JavaScript Document
